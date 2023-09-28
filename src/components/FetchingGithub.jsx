c0a6f6a77043ac8ea57f0dede964f2dc2a06c082
Valentina Valverde <vvgreenville@gmail.com>
2023-09-28 14:58:28 -0400

    Initial commit (?)

--- src/components/FetchingGithub.jsx
+++ src/components/FetchingGithub.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 export const FetchGithub = () => {
     const [username, setUsername] = useState('')
     const [userData, setUserData] = useState({})
 
-   
+
 
     const getProfile = async () => {
         const response = await fetch(`https://api.github.com/users/${username}`);
@@ -13,9 +13,6 @@ export const FetchGithub = () => {
         console.log("DATA: ", data);
     }
     console.log("USERNAME: ", userData);
-
-    //i want name, login, company, location, bio, followers, following (and eventuall avatar_url)
-
 
     const handleChange = (event) => {
         setUsername(event.target.value)
@@ -28,24 +25,30 @@ export const FetchGithub = () => {
     }
 
 
-    return(
+    return (
         <>
-        <p>Type in a username to generate info:</p>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="username" onChange={(event) => {handleChange(event)}}></input>
-            <br />
-            <button type="submit">Load Profile</button>
-        </form>
+            <p>Type in a username to generate info:</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="username" value="" onChange={(event) => { handleChange(event) }}></input>
+                <br />
+                <button type="submit">Load Profile</button>
+            </form>
 
-        <br />
-        <img src={userData.avatar_url}></img>
-        <p>Name: {userData.name}</p>
-        <p>Login: {userData.login}</p>
-        <p>Company: {userData.company}</p>
-        <p>Location: {userData.location}</p>
-        <p>Bio: {userData.bio}</p>
-        <p>Followers: {userData.followers}</p>
-        <p>Following: {userData.following}</p>
+        <div className="output">
+            <br />
+            <img src={userData.avatar_url}></img>
+            <p>Name: {userData.name}</p>
+            <p>Login: {userData.login}</p>
+            <p>Company: {userData.company}</p>
+            <p>Location: {userData.location}</p>
+            <p>Bio: {userData.bio}</p>
+            <p>Followers: {userData.followers}</p>
+            <p>Following: {userData.following}</p>
+        </div>
+
+            <form>
+                <button type="submit">Reset</button>
+            </form>
 
 
         </>
