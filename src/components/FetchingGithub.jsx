ccfa8a41aab412e3ae27b47e20197809f176eb06
Valentina Valverde <vvgreenville@gmail.com>
2023-09-30 17:00:19 -0400

    Fixed bug

--- src/components/FetchingGithub.jsx
+++ src/components/FetchingGithub.jsx
@@ -2,7 +2,7 @@ import { useState, Fragment } from "react";
 
 export const FetchGithub = () => {
     const [username, setUsername] = useState('')
-    const [userData, setUserData] = useState({})
+    const [userData, setUserData] = useState([])
 
 
 
@@ -16,7 +16,8 @@ export const FetchGithub = () => {
 
     const handleChange = (event) => {
         setUsername(event.target.value)
-    }
+        console.log("EVENT TARGET VALUE: ", event.target.value);
+}
 
 
     const handleSubmit = (event) => {
@@ -28,23 +29,25 @@ export const FetchGithub = () => {
     return (
         <>
             <p>Type in a username to generate info:</p>
+
+
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="username" value="" onChange={(event) => { handleChange(event) }}></input>
+                <input type="text" placeholder="username" onChange={(event) => { handleChange(event) }}></input>
                 <br />
                 <button type="submit">Load Profile</button>
             </form>
 
-        <div className="output">
-            <br />
-            <img src={userData.avatar_url}></img>
-            <p>Name: {userData.name}</p>
-            <p>Login: {userData.login}</p>
-            <p>Company: {userData.company}</p>
-            <p>Location: {userData.location}</p>
-            <p>Bio: {userData.bio}</p>
-            <p>Followers: {userData.followers}</p>
-            <p>Following: {userData.following}</p>
-        </div>
+            <div className="output">
+                <br />
+                <img src={userData.avatar_url}></img>
+                <p>Name: {userData.name}</p>
+                <p>Login: {userData.login}</p>
+                <p>Company: {userData.company}</p>
+                <p>Location: {userData.location}</p>
+                <p>Bio: {userData.bio}</p>
+                <p>Followers: {userData.followers}</p>
+                <p>Following: {userData.following}</p>
+            </div>
 
             <form>
                 <button type="submit">Reset</button>
